d68e00db97ae95d2a8a52ae686d034300d31be6a
ishikadoultani5-gif <ishikadoultani5@gmail.com>
2026-02-03 21:11:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function addTask() {
+  let task = document.getElementById("taskInput").value;
+  if (task === "") return;
+
+  let li = document.createElement("li");
+  li.innerText = task;
+  document.getElementById("taskList").appendChild(li);
+
+  document.getElementById("taskInput").value = "";
+}
